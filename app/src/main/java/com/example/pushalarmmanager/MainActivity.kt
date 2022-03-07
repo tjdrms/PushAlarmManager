10aede8d7b264185fa87fdea6133b702e26e4f65
tjdrms <tjdrms0116>
2022-03-07 11:37:32 +0900

    간단한 푸쉬 알람 수신기

---
+++ app/src/main/java/com/example/pushalarmmanager/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.pushalarmmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.google.firebase.messaging.FirebaseMessaging
+
+class MainActivity : AppCompatActivity() {
+
+    private val resultTextView: TextView by lazy {
+        findViewById(R.id.resultTextView)
+    }
+
+    private val firebaseTokenTextView: TextView by lazy {
+        findViewById(R.id.firebaseTokenTextView)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initFirebase()
+    }
+
+    private fun initFirebase() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                firebaseTokenTextView.text = task.result
+            }
+        }
+
+    }
+}
